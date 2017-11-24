9ef7916803db5ed332cacefa8bd73e130b875963
Anthony Rozon <arozonm@gmail.com>
2017-11-24 10:22:45 -0400

    Added conftest file

---
+++ conftest.py
@@ -0,0 +1,50 @@
+import pytest
+import os
+
+
+@pytest.fixture
+def browser():
+    return pytest.config.getoption("-B")
+
+
+@pytest.fixture
+def browserstack_flag():
+    return pytest.config.getoption("-M")
+
+
+@pytest.fixture
+def browser_version():
+    return pytest.config.getoption("-V")
+
+
+@pytest.fixture
+def platform():
+    return pytest.config.getoption("-P")
+
+
+@pytest.fixture
+def os_version():
+    return pytest.config.getoption("-O")
+
+
+def pytest_addoption(parser):
+    parser.addoption("-B", "--browser",
+                     dest="browser",
+                     default="chrome",
+                     help="Browser. Default option is chrome")
+    parser.addoption("-M", "--browserstack_flag",
+                     dest="browserstack_flag",
+                     default="N",
+                     help="Run the test in Browserstack: Y or N")
+    parser.addoption("-V", "--ver",
+                     dest="browser_version",
+                     default="45",
+                     help="Browser version")
+    parser.addoption("-P", "--platform",
+                     dest="platform",
+                     default="Windows",
+                     help="OS Platform: Windows, Linux")
+    parser.addoption("-O", "--os_version",
+                     dest="os_version",
+                     default="7",
+                     help="OS Version: XP, 7, 10")
