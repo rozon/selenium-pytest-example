43aab19fd632f43e328bddfb40352117507425bc
Anthony Rozon <arozonm@gmail.com>
2017-11-24 22:34:35 -0400

    Added print statements for debugging purposes

--- test_github_search.py
+++ test_github_search.py
@@ -62,9 +62,12 @@ def test_github_user_search(browser, browserstack_flag, browser_version, platfor
     # Check if github results returns same amount of users
     with pytest.allure.step('Check if github results returns same amount of users'):
         label_users_search_count = driver.find_element_by_xpath(".//div[contains(@class, 'three-fourths')]/div[1]/h3")
-        label = label_users_search_count.text
+        label = label_users_search_count.get_attribute('text')
+        print(label)
         label_number = label[0:2]
+        print(label_number)
         label_to_int = int(label_number)
+        print(label_to_int)
 
         if github_user_results_count == label_to_int:
             pass_counter += 1
